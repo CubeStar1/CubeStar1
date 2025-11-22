32e58aad9df682196dfbc895a5519ab662a09606
Avinash Anish <143164352+CubeStar1@users.noreply.github.com>
2025-11-22 21:52:41 +0530

    Update API URL to new endpoint

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Avinash%20Anish&g=CubeStar1&x=thecubestar&l=avinash-anish&i=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F143164352%3Fs%3D400%26u%3D54634695f746486bd8b4d501683c9e80d83cb538%26v%3D4&p=https%3A%2F%2Favinash.thecubestar.com&z=81d8d";
+const apiUrl = "https://opbento.vercel.app/api/bento?n=Avinash%20Anish&g=CubeStar1&x=thecubestar&l=avinash-anish&i=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F143164352%3Fs%3D400%26u%3D54634695f746486bd8b4d501683c9e80d83cb538%26v%3D4&p=https%3A%2F%2Favinash.thecubestar.com&z=81d8d";
 interface BentoResponse {
   url: string;
 }
